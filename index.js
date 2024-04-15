4301b848f26a49cd6ba948932ee8b67120ae149f
Victor Nguyo <victor.nguyo@student.moringaschool.com>
2024-04-15 10:23:55 +0300

    completelab

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word) {
+  return word.split("").reverse().join("");
 }
 
-/* 
-  Add your pseudocode here
-*/
+function isPalindrome(word) {
+  const reversedWord = reverse(word);
+  return word === reversedWord;
+}
 
 /*
-  Add written explanation of your solution here
+  Add your pseudocode here
+  that means if the word is the same as the word in reverse, I should return true.
+  
+  reverse the input string
+  if the input is the same as the reversed input
+    return true
+  else
+    return false
 */
 
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
 
   console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome(""));
 }
 
 module.exports = isPalindrome;
